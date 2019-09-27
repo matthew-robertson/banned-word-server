e60a9c5885c37ff66912f4f7912cfeb5d258d602
matthew-robertson <m33rober@uwaterloo.ca>
2019-09-26 22:08:42 -0400

    Adds MessageRoute tests
    This should finish up the resource level tests. Still need to do model
    level stuff, and end-point level things, but those can wait. Next up,
    CI.

--- bannedWordServer/routes/messageroute.py
+++ bannedWordServer/routes/messageroute.py
@@ -1,8 +1,13 @@
+import re
+
 from bannedWordServer.models.ban import Ban
 from bannedWordServer.routes.resource import Resource
-from bannedWordServer.constants.errors import NotFoundError
+from bannedWordServer.constants.errors import NotFoundError, InvalidTypeError, ValidationError
 
 class MessageRoute(Resource):
+	def __init__(self):
+		self.pattern = re.compile(r"[0-9]*-[0-9]*-[0-9]* [0-9]*:[0-9]*:[0-9]*")
+
 	def get_collection(self, session, id):
 		pass
 
@@ -11,11 +16,15 @@ class MessageRoute(Resource):
 
 	def post(self, session, requestJson) -> dict:
 		banid = requestJson['ban_id']
-		ban_to_modify = session.query(Ban).filter(rowid=banid).first()
+		if not isinstance(banid, int): raise InvalidTypeError
+		ban_to_modify = session.query(Ban).filter_by(rowid=banid).first()
 		if not ban_to_modify: raise NotFoundError
 
+		if not isinstance(requestJson['sent_time'], str): raise InvalidTypeError
+		if not self.pattern.match(requestJson['sent_time']): raise ValidationError
+
 		ban_to_modify.infracted_at = requestJson['sent_time']
-		if (requestJson['called_out']):
+		if ('called_out' in requestJson.keys()):
 			ban_to_modify.calledout_at = requestJson['sent_time']
 
-		return session.query(Ban).filter(rowid=banid).first().to_dict()
+		return session.query(Ban).filter_by(rowid=banid).first().to_dict()
---
+++ tests/routes/test_messageroute.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from unittest import TestCase
+
+from bannedWordServer.constants.errors import NotFoundError, InvalidTypeError, ValidationError
+from bannedWordServer.models import Base
+from bannedWordServer.models.server import Server
+from bannedWordServer.models.ban import Ban
+from bannedWordServer.routes.messageroute import MessageRoute
+
+Session = sessionmaker()
+engine = create_engine('sqlite:///:memory:')
+
+class TestBanRoutePostOne(TestCase):
+	def setUp(self):
+		Base.metadata.create_all(engine)
+		self.connection = engine.connect()
+		self.trans = self.connection.begin()
+		self.session = Session(bind=self.connection)
+		serverid = 1234
+		new_ban = Ban(server_id=serverid, banned_word="asdf",\
+			infracted_at=(datetime.now()-timedelta(days=6)).strftime("%Y-%m-%d %H:%M:%S"),\
+			calledout_at=(datetime.now()-timedelta(days=6)).strftime("%Y-%m-%d %H:%M:%S"))
+		new_server = Server(server_id=serverid, banned_words=[new_ban])
+		self.session.add(new_server)
+
+	def test_messageroute_post__ban_not_found(self):
+		current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		request = {'ban_id': 3, 'sent_time': datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+		self.assertRaises(NotFoundError, MessageRoute().post, self.session, request)
+
+	def test_messageroute_post__good_request(self):
+		current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		request = {'ban_id': 1, 'sent_time': datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+		self.assertNotEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().infracted_at)
+		result = MessageRoute().post(self.session, request)
+		self.assertEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().infracted_at)
+		self.assertNotEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().calledout_at)
+
+	def test_messageroute_post__good_request_callout(self):
+		current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		request = {'ban_id': 1, 'sent_time': current_time, 'called_out': True}
+		self.assertNotEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().infracted_at)
+		self.assertNotEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().calledout_at)
+
+		result = MessageRoute().post(self.session, request)
+		self.assertEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().infracted_at)
+		self.assertEqual(current_time, self.session.query(Ban).filter_by(rowid=1).first().calledout_at)
+
+	def test_messageroute_post__invalid_paras(self):
+		current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		request = {'ban_id': "asdf", 'sent_time': current_time, 'called_out': True}
+		self.assertRaises(InvalidTypeError, MessageRoute().post, self.session, request)
+
+		request = {'ban_id': 1, 'sent_time': 1234, 'called_out': True}
+		self.assertRaises(InvalidTypeError, MessageRoute().post, self.session, request)
+
+		request = {'ban_id': 1, 'sent_time': 'asdf', 'called_out': True}
+		self.assertRaises(ValidationError, MessageRoute().post, self.session, request)
+
+	def tearDown(self):
+		self.session.close()
+		self.trans.rollback()
+		self.connection.close()

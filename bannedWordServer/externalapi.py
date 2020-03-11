ca348c14febc8b38cb6ea628c412205043fc1ca9
matthew-robertson <m33rober@uwaterloo.ca>
2020-03-10 21:20:49 -0400

    Fixes getManageableDiscordServers
    On error fetching user's server list, wasn't handling it correctly. When
    the happy path was changed to be a multiple return, the error path
    wasn't updated.

--- bannedWordServer/externalapi.py
+++ bannedWordServer/externalapi.py
@@ -6,7 +6,7 @@ from bannedWordServer.config import DISCORD_BASE_URL
 def getManageableDiscordServers(auth_token):
 	result = requests.get(DISCORD_BASE_URL + 'users/@me/guilds',
 	headers = {'Authorization': auth_token})
-	if (not result.status_code == 200): return []
+	if (not result.status_code == 200): return [], result.status_code
 	return list(filter(lambda server: (server['permissions'] & 0x00000008) == 0x00000008, json.loads(result.content))), result.status_code
 
 def getDiscordUser(auth_token):
